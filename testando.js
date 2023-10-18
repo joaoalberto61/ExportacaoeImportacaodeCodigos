ffa42029209c3aa0f236a8e79f156fd10dcd8ff8
João Alberto <joaoalberto61@gmail.com>
2023-10-18 11:59:19 -0300

    Add files via upload

---
+++ testando.js
@@ -0,0 +1,5 @@
+function mensagem(){ //Função "mensagem"
+    return 'Testando importação de códigos.'; //Mensagem que vai ser enviada para o "App.js"
+}
+
+export default mensagem; //Exportanto a função "mensagem"
